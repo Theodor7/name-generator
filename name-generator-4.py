aa58f6f610612bdee05c9577ad1945ceda9490df
Theodor7 <theodor.b.hansen@gmail.com>
2016-10-11 22:57:12 +0200

    From 545 bytes to ONLY 542 bytes! - Optimization

--- name-generator-4.py
+++ name-generator-4.py
@@ -1,8 +1,5 @@
 from random import randint, choice
-vowels = 'aaaaaaaaaaaaaeeeeeeeeeeeeeiiiiiiiiioooooooooouuuuyy'
-consonants = 'bbbbbbbbcccddddddffffffggghhhjjkkkkkkkllllllmmmmmmmmnnnnnnnppppppqrrrrrrsssssssssstttttttvvvvwxz'
-name = ''
-pattern = randint(0,1)
+vowels, consonants, name, pattern = 'aaaaaaaaaaaaaeeeeeeeeeeeeeiiiiiiiiioooooooooouuuuyy', 'bbbbbbbbcccddddddffffffggghhhjjkkkkkkkllllllmmmmmmmmnnnnnnnppppppqrrrrrrsssssssssstttttttvvvvwxz', '', randint(0,1)
 for x in range(0,randint(3,10)):
     if x %2 == pattern:
         name += choice(vowels)
