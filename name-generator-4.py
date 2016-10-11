d77ce3306482c56f8d7b462c5f7a2c1e06cd8b65
Theodor7 <theodor.b.hansen@gmail.com>
2016-10-11 22:47:58 +0200

    Consonants less likely, vowels more so

--- name-generator-4.py
+++ name-generator-4.py
@@ -6,10 +6,10 @@ pattern = randint(0,1)
 for x in range(0,randint(3,10)):
     if x %2 == pattern:
         name += choice(vowels)
-        if randint(0,8) == 0:
+        if randint(0,7) == 0:
             pattern = -(pattern-1)
     else:
         name += choice(consonants)
-        if randint(0,3) == 0:
+        if randint(0,4) == 0:
             pattern = -(pattern-1)
 print name.capitalize()
