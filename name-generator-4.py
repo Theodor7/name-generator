2b003118846c11f95ba27b5f340c5a39b191676a
Theodor7 <theodor.b.hansen@gmail.com>
2016-10-11 17:04:12 +0200

    DID SOMETHING INCREDIBLY SMART - Optimization
    name-generator-4 is now the file that takes up the least space (less
    than 3).

--- name-generator-4.py
+++ name-generator-4.py
@@ -7,15 +7,9 @@ for x in range(0,random.randint(3,10)):
     if x %2 == pattern:
         name += random.choice(vowels)
         if random.randint(0,8) == 0:
-            if pattern == 0 :
-                pattern = 1
-            else:
-                pattern = 0
+            pattern = -(pattern-1)
     else:
         name += random.choice(consonants)
         if random.randint(0,4) == 0:
-            if pattern == 0 :
-                pattern = 1
-            else:
-                pattern = 0
+            pattern = -(pattern-1)
 print name.capitalize()
